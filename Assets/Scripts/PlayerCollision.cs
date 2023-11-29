fda60a7c4f950f7bf4c06e214c806447dae8726b
Devin Keller <Dkeller@chapman.edu>
2023-11-28 20:21:59 -0800

    Added enemy and if you touch enemy you lose a life

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public int lives = 3; // Number of lives the player has
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision Detected with " + collision.gameObject.name);
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            lives--;
+            Debug.Log("Lives left: " + lives);
+            if (lives <= 0)
+            {
+                Debug.Log("Game Over!");
+                // Add game over logic here
+            }
+        }
+    }
+    
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,12 +8,12 @@ e. Assignment 1
 
 using UnityEngine;
 
-public class Move : Command
+public class PlayerMovement : Command
 {
     private float moveDistance = 1f;
     private Vector2 direction;
 
-    public Move(Vector3 dir)
+    public PlayerMovement(Vector3 dir)
     {
         direction = dir;
     }
@@ -22,5 +22,12 @@ public class Move : Command
     {
         // Move character
         gameObject.transform.Translate(direction * moveDistance);
+    }
+    
+    public void Respawn()
+    {
+        // Respawn logic
+        // this.transform.position = new Vector3(0, 0, 0); // Example respawn position
+        // Add invincibility time or other respawn logic here
     }
 }
