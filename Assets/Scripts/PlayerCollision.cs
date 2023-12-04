454558e3a58214bfccb3361601688ea26c229034
kentamats <113066384+kentamats@users.noreply.github.com>
2023-12-04 04:51:59 -0800

    Revert "Merge from main"
    This reverts commit 147acd25b5d34de3ffed23df578f6514573b9927, reversing
    changes made to 6034009f6e209b106263e2bb3b535c893e6c307b.

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -1,14 +1,8 @@
 using UnityEngine;
-using TMPro;
 
 public class PlayerCollision : MonoBehaviour
 {
     public int lives = 3; // Number of lives the player has
-<<<<<<< HEAD
-=======
-    public Transform respawnPoint; // The respawn point
-    public TextMeshProUGUI livesText;
->>>>>>> main
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -16,17 +10,8 @@ public class PlayerCollision : MonoBehaviour
         if (collision.gameObject.CompareTag("Enemy"))
         {
             lives--;
-<<<<<<< HEAD
             Debug.Log("Lives left: " + lives);
             if (lives <= 0)
-=======
-            livesText.text = "Lives: " + lives;
-            if (lives > 0)
-            {
-                Respawn();
-            }
-            else
->>>>>>> main
             {
                 Debug.Log("Game Over!");
                 // Add game over logic here
